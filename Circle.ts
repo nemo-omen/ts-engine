4773642d2dd522eb1e2059f83d01fe7517bb4316
nemo-omen <nemo.omen@gmail.com>
2022-12-07 11:25:18 -0600

    Finicky typescript stuff

--- Circle.ts
+++ Circle.ts
@@ -1,14 +1,14 @@
 export class Circle {
   ctx: CanvasRenderingContext2D;
-  radius: number;
-  cx: number;
-  cy: number;
-  origin: number[] | undefined;
+  radius: number | undefined;
+  cx: number | undefined;
+  cy: number | undefined;
+  origin: number[] | undefined[] | undefined;
   fillStyle: string | undefined;
   strokeStyle: string | undefined;
   strokeWidth: number | undefined;
 
-  constructor(ctx: CanvasRenderingContext2D, cx: number, cy: number, radius: number) {
+  constructor (ctx: CanvasRenderingContext2D, cx: number, cy: number, radius: number) {
     this.ctx = ctx;
     this.init(cx, cy, radius);
   }
@@ -16,18 +16,18 @@ export class Circle {
   init(cx: number, cy: number, radius: number) {
     this.cx = cx;
     this.cy = cy;
+    this.origin = [cx, cy];
     this.radius = radius;
   }
-  
+
   distance(dest: number[], pos: number[]) {
-    return Math.sqrt(Math.pow((dest[0] - pos[0]), 2), Math.pow((dest[1] - pos[1]), 2));
+    return Math.sqrt(Math.pow((dest[0] - pos[0]), 2) + Math.pow((dest[0] - pos[0]), 2));
   }
 
   move(destX: number, destY: number, deltaTime: number) {
-    this.origin[0] = this.cx;
-    this.origin[1] = this.cy;
-    this.cx += this.lerp(origin[0], this.destX, deltaTime);
-    this.cy += this.lerp(origin[1], this.destY, deltaTime);
+    this.origin = [this.cx!, this.cy!];
+    this.cx! += this.lerp(this.origin[0], destX, deltaTime);
+    this.cy! += this.lerp(this.origin[1], destY, deltaTime);
   }
 
   lerp(a: number, b: number, t: number): number {
@@ -35,17 +35,17 @@ export class Circle {
   }
 
   render(fillStyle: string | undefined,
-         strokeStyle: string | undefined = undefined,
-         lineWidth: number | undefined = undefined) {
+    strokeStyle: string | undefined = undefined,
+    lineWidth: number | undefined = undefined) {
     this.ctx.beginPath();
-    this.ctx.arc(this.cx, this.cy, this.radius, 0, 2 * Math.PI, false);
-    console.log(fillStyle); 
-    if(fillStyle !== undefined) {
+    this.ctx.arc(this.cx!, this.cy!, this.radius!, 0, 2 * Math.PI, false);
+    console.log(fillStyle);
+    if (fillStyle !== undefined) {
       this.ctx.fillStyle = fillStyle;
       this.ctx.fill();
     }
 
-    if(strokeStyle !== undefined && lineWidth !== undefined) {
+    if (strokeStyle !== undefined && lineWidth !== undefined) {
       this.ctx.lineWidth = lineWidth;
       this.ctx.strokeStyle = strokeStyle;
       this.ctx.stroke();
